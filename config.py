ce820a70ea856bcf6eaacb75ccf7c0c9265acd33
sunhanwu <1965190613@qq.com>
2020-12-07 23:34:13 +0800

    1. completely define the basic mysql table in database.py 2. completely find 20 dns servers in dns_servers.txt 3. define mysql database connection config in config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+db_config = {
+        'host': 'www.sunhanwu.top',
+        'port': 3306,
+        'user': 'cdn_user',
+        'password': 'cdn_123456',
+        'database': 'cdn'
+}
--- database/database.py
+++ database/database.py
@@ -1,7 +1,12 @@
+"""
+Author: sunhanwu
+Date: 2020-12-07
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, Date, Enum
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship, backref, sessionmaker
+from config import db_config
 
 Base = declarative_base()
 
@@ -10,8 +15,39 @@ class CNAME(Base):
     对应CNAME表结构
     """
     __tablename__ = 'CNAME'
-    # CNAME 记录id
+    # id，主键，自增
     id = Column(Integer, autoincrement=True, primary_key=True)
-    dns = Column(String(65))
-    ip = Column(String(20))
+    # dns 域名1，限制长度在65个字符之内
+    dns1 = Column(String(65))
+    # dns 域名2, 限制长度在65个字符内
+    dns2 = Column(String(65))
+    # 查询的递归服务器位置
     area = Column(Enum(""))
+    # 插入数据的时间
+    date = Column(Date)
+
+class A(Base):
+    """
+    对应A表结构
+    """
+    __tablename__ = 'A'
+    # id，主键，自增
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    # dns 域名，限制长度在65个字符之内
+    dns = Column(String(65))
+    # 具体的ip
+    ip = Column(String(20))
+    # 查询的递归服务器位置
+    area = Column(Enum(""))
+    # ip前24位的值
+    ip_24 = Column(String(20))
+    # dns 递归链深度
+    depth = Column(Integer)
+    # 插入数据的时间
+    date = Column(Date)
+
+engine = create_engine('mysql+mysqlconnector://{}:{}@{}:{}/{}?charset=utf8'.format( db_config['user'], db_config['password'], db_config['host'], db_config['port'], db_config['database']))
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+
+
