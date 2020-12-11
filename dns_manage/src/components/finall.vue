23dcddc31f10d846568341772b8e97dcd8a52ced
李志平 <786206401@qq.com>
2020-12-11 18:16:09 +0800

    add new neo4j-vue

--- dns_manage/src/components/finall.vue
+++ dns_manage/src/components/finall.vue
@@ -1,17 +1,15 @@
 <template>
     <div>
-        <el-row>
+        <el-row>  
         <el-card>
         <!-- <div id="toolbar">
             <div class="ibox-title">
                 <h5>查询结果</h5>
             </div>
-        </div> -->
-            
+        </div> -->   
         <el-input v-model="input" placeholder="请输入CDN域名" style="display:inline-table; width: 10%; float:left" @input.native="btn()" ></el-input>
           <table
             id="CDN"
-            data-height="800"
             class="table table-bordered table-striped"
             data-classes="table table-hover table-condensed"
             data-mobile-responsive="true"
@@ -28,21 +26,25 @@
             data-click-to-select="true">
             <thead>
                 <tr>
-                <th data-field="rawdomain">原始域名</th>
-                <th data-field="cdndomain">CDN域名</th>
-                <th data-field="region">地区</th>
-                <th data-field="ipaddress">IP</th>
+                <th data-field="domain_name" >原始域名</th>
+                <th data-field="ip_addr">CDN_IP</th>
+                <th data-field="depth">深度</th>
+                <th data-field="recur_server">地区</th>
                 </tr>
             </thead>
           </table>
             <!--data-url="/dns-serve/api/domainExhibition/blacklist/whitelisttop10"/>-->       
         </el-card>
         </el-row>
+        <el-row>    
+          <div class="mynetwork" id="mynetwork"></div>
+        </el-row>  
      </div>   
 </template>
 
 <script>
 import $ from 'jquery'
+import vis from 'vis'
 export default {
     name:'finall',
     data(){
@@ -53,7 +55,7 @@ export default {
           input:'',
           columnsWhite: [
           {
-            field: 'rawdomain', // 序号'
+            field: 'domain_name', // 序号'
             // type:'index',
             title: '原始域名',
             align: 'center', // 对其方式
@@ -61,53 +63,39 @@ export default {
             // sortable: true
           },
           {
-            field: 'cdndomain',
-            title: 'CDN域名',
+            field: 'ip_addr',
+            title: 'CDN_IP',
             valign: 'middle',
             halign: 'center',
             align: 'center',
             // sortable: true
           },
           {
-            field: 'region',
-            title: '地区',
+            field: 'depth',
+            title: '深度',
             valign: 'middle',
             halign: 'center',
             align: 'center',
             // sortable: true
           },
           {
-            field: 'ipaddress',
-            title: 'IP',
+            field: 'recur_server',
+            title: '递归服务器IP',
             valign: 'middle',
             halign: 'center',
             align: 'center',
             // sortable: true
           },
         ],
+        nodes : [],
+        edges : [],
+              options: {},
         }
     },
-    mounted() {        
-        // $('#CDN').bootstrapTable('destroy').bootstrapTable({
-        //     showExport: true,
-        //     exportDataType: 'all',
-        //     exportTypes: ['json', 'xml', 'csv', 'txt', 'sql', 'excel', 'doc'],
-        //     columns: this.columnsWhite,
-        //     // data: this.data,
-        //     showToggle: true,
-        //     responseHandler(res) {
-        //     console.log('1234');
-        //     return {
-        //         "rows": res.rows,
-        //         "total": res.total
-        //     }
-        //     },
-        //     onLoadSuccess(res) {
-        //     // console.log(res);
-        //     },
-        // });
+    mounted() { 
+        this.test();
         this.getlist();
-
+        this.init();
   },
     methods: {
     export1(data){
@@ -115,57 +103,224 @@ export default {
     },
     searchnews(){
       this.search=this.$route.query.search
-      console.log(this.search)  
-      // this.$http.get('http://127.0.0.1:8000/api/add?search=' + this.search)
-      //   .then((response) => {
-      //     console.log('7',response)
-      //     var res = JSON.parse(response.bodyText)
-      //     this.bookList=res.msg
-      //     console.log('1',this.bookList)
-      //   })
-        this.data = [{rawdomain:'1',cdndomain:'www.baidu.com',region:'China',ipaddress:'192.123.123.1'}]
-        // $("#CDN").bootstrapTable('resetSearch', this.search);  
+      console.log(this.search)
+        this.data = [{rawdomain:'1',cdndomain:'www.baidu.com',region:'China',ipaddress:'192.123.123.1'}]        
         $('#CDN').bootstrapTable("load", this.data)
     },
     getlist(){
-        this.data = [{rawdomain:'1',cdndomain:'www.baidu.com',region:'China',ipaddress:'192.123.123.1'},{rawdomain:'2',cdndomain:'www.google.com',region:'America',ipaddress:'192.123.23.2'}]
-        // $("#CDN").bootstrapTable('resetSearch', this.search);  
-        // $('#CDN').bootstrapTable("load", this.data)
         this.search=this.$route.query.search
         this.$http.get('http://127.0.0.1:8000/query/?domain=' + this.search)
         .then((response) => {
           console.log('7',response)
           var res = JSON.parse(response.bodyText)
           this.bookList=res.msg[0]
-          console.log('1',this.bookList) 
+          console.log('1',this.bookList)
+          this.bookList.forEach((i,index)=>{
+            // console.log(i)
+            if(i.recur_server=="219.141.140.10"){
+              i.recur_server="北京"
+            }
+            if(i.recur_server=="8.8.8.8"){
+              i.recur_server="加州"
+            }
+            if(i.recur_server=="8.8.4.4"){
+              i.recur_server="美国"
+            }
+            if(i.recur_server=="91.239.100.100"){
+              i.recur_server="丹麦"
+            }
+            if(i.recur_server=="77.88.8.8"){
+              i.recur_server="俄罗斯"
+            }
+            if(i.recur_server=="80.80.80.80"){
+              i.recur_server="荷兰"
+            }
+            if(i.recur_server=="221.11.1.67"){
+              i.recur_server="陕西"
+            }
+            if(i.recur_server=="210.22.70.3"){
+              i.recur_server="上海"
+            }
+            if(i.recur_server=="79.141.82.250"){
+              i.recur_server="瑞士"
+            }
+            if(i.recur_server=="61.132.163.68"){
+              i.recur_server="安徽"
+            }
+            if(i.recur_server=="45.248.197.53"){
+              i.recur_server="澳大利亚"
+            }
+            if(i.recur_server=="168.95.1.1"){
+              i.recur_server="台湾"
+            }
+            if(i.recur_server=="202.14.67.4"){
+              i.recur_server="香港"
+            }
+            if(i.recur_server=="210.22.70.3"){
+              i.recur_server="上海"
+            }
+            if(i.recur_server=="45.248.197.5"){
+              i.recur_server="澳大利亚"
+            }
+            if(i.recur_server=="168.95.1.1"){
+              i.recur_server="台湾"
+            }
+            if(i.recur_server=="202.14.67.4"){
+              i.recur_server="香港"
+            }
+            if(i.recur_server=="84.200.69.80"){
+              i.recur_server="德国"
+            }
+            if(i.recur_server=="223.6.6.6"){
+              i.recur_server="杭州"
+            } 
+            if(i.recur_server=="165.87.13.129"){
+              i.recur_server="德州"
+            } 
+          }),
+          $('#CDN').bootstrapTable({
+            data: this.bookList
+            }) 
          })
-
-
-        console.log(this.search) 
-        $('#CDN').bootstrapTable({
-            data: this.data
-            })
-        // console.log(this.data)
     },
     btn:function(){
-      console.log(this.input)        
+      // console.log(this.input)        
       var search = this.input;
       if (search) {
-        this.searchData = this.data.filter(function(product) {
+        this.searchData = this.bookList.filter(function(product) {
           // console.log('1',product)
           return Object.keys(product).some(function(key) {
             // console.log('2',key)
-            return String(product['cdndomain']).toLowerCase().indexOf(search) > -1
+            return String(product['ip_addr']).toLowerCase().indexOf(search) > -1
           })
         })
         $('#CDN').bootstrapTable("load", this.searchData)  
       }
       else{
-        $('#CDN').bootstrapTable("load", this.data)  
+        $('#CDN').bootstrapTable("load", this.bookList)  
       }          
-      }
+      },
+    init(){
+			          let _this = this;
+			          var container = document.getElementById("mynetwork");
+			          var data = {
+			            nodes: _this.nodes,
+			            edges: _this.edges,
+			          };		
+			          _this.options = {
+			            edges: {
+			                    width: 3,
+			                    length: 200,
+			                    shadow: true,
+			                    arrows: { 
+			                      to: {
+			                        enabled: true,
+			                        scaleFactor: 1,  
+			                        type: "arrow"
+			                      },
+                           }, 
+                           color: {
+			                      color: "#bc68e6",
+			                      highlight: "yellow",
+			                      hover: "#1fe1c6",
+			                      inherit: "from",
+			                      opacity: 1.0
+			                    },
+			                    font: {
+			                      size: 14, 
+			                      face: 'arial',
+			                      background: 'white',
+			                      strokeWidth: 10,
+			                      strokeColor: 'rgb(112, 184, 240)',
+			                      align: 'horizontal',
+			                      multi: false,
+			                      vadjust: 0,
+			                      bold: {
+			                        color: "green",
+			                      },
+			                   },
+			            },
+			            nodes: {
+			              shape: 'circularImage',
+			              font: {
+			                bold: {
+			                  color: "red",
+			                },
+			              },
+			            },
+			            physics: {
+			              enabled: false,
+			            },
+			            interaction: {
+			              hover: true,
+			              dragNodes: true, //是否能拖动节点
+			              dragView: true, //是否能拖动画布
+			              hover: true, //鼠标移过后加粗该节点和连接线
+			              multiselect: true, //按 ctrl 多选
+			              selectable: true, //是否可以点击选择
+			              selectConnectedEdges: true, //选择节点后是否显示连接线
+			              hoverConnectedEdges: true, //鼠标滑动节点后是否显示连接线
+			              zoomView: true //是否能缩放画布
+			            },
+                  physics: {
+                    enabled: true,
+                    barnesHut: {
+                        gravitationalConstant: -10000,
+                        centralGravity: 0.3,
+                        springLength: 120,
+                        springConstant: 0.04,
+                        damping: 0.09,
+                        avoidOverlap: 0
+                    }
+                }
+			          };
+			       _this.network = new vis.Network(container, data,  _this.options);
+          },
+    test(){
+      this.nodes=  [
+			            { id: 1, 
+			              label: "This is a\nsingle-font label",
+			              image:'@/image.jpg',
+			              title:'space',  
+                        },
+			            {
+			              id: 2,
+			              font: { multi: true },
+			              label: "<b>This</b> is a\n<i>default</i> <b><i>multi-</i>font</b> <code>label</code>",
+			              image:'@/image.jpg',
+			              title:'earth',  
+			            },
+			            {
+			              id: 3,
+			              font: { multi: "html", size: 20 },
+			              label: "<b>This</b> is an\n<i>html</i> <b><i>multi-</i>font</b> <code>label</code>",
+			              image:'@/image.jpg',
+			              title:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1924443930,1818184200&fm=26&gp=0.jpg',  
+			            },
+			            {
+			              id: 4,
+			              label: "markdown",
+			              image:'@/image.jpg',
+			              title:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2299626866,2306729894&fm=26&gp=0.jpg',  
+			            },
+			            {
+			              id: 5,
+			              font: { multi: "md", face: "georgia", },
+			              label: "*This* is a\n_markdown_ *_multi-_ font* `label`",
+			              image:'@/image.jpg',   
+			              title:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3443312632,4280189108&fm=26&gp=0.jpg',        
+			            },
+              ];
+      this.edges =[
+			            { from: 1, to: 2, label: "single to default" },
+			            { from: 2, to: 3, font: { multi: true }, label: "default to <b>html</b>" },
+			            { from: 2, to: 4, font: { multi: "md" }, label: "*html* to _md_" },
+			            { from: 2, to: 5, font: { multi: "md" }, label: "*html* to _md_" },
+			        ];
+    }
+			  }
+      
   }
-}
 </script>
 
 <style scoped lang="scss">
@@ -173,7 +328,6 @@ export default {
     background-color: #fff;
     border-color: #e7eaec;
     color: inherit;
-
     h5 {
       display: inline-block;
       font-size: 16px;
@@ -184,4 +338,11 @@ export default {
       color: #007bff;
     }
   }
+  .mynetwork {
+			  height: 800px;
+			  border: 0px solid lightgray;
+			  canvas{
+			    cursor: pointer;
+			  }
+		}
 </style>
